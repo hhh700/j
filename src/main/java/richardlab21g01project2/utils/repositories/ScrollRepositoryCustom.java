7904f12f882344a8516737f47e998199ddc21d2b
hhh700 <1493129978@qq.com>
2024-10-18 14:31:50 +1100

    Delete src directory

--- src/main/java/richardlab21g01project2/utils/repositories/ScrollRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package richardlab21g01project2.utils.repositories;
-import java.util.*;
-import richardlab21g01project2.utils.entities.*;;
-public interface ScrollRepositoryCustom {
-    List<Scroll> findAllScrolls();
-}
-
